8f019219064968303d0707f2365141c08224c0ac
IvanSemenkin <ismenkin@gmail.com>
2024-12-03 18:08:41 +0300

    Prepare for Vercel deployment

---
+++ api/index.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+# Добавляем путь к корневой директории проекта
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from app import app as application
+
+def handler(event, context):
+    return application
